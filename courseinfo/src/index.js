6a1f5c705090a067f8315f6da05f3bdf66da480c
Joe Lott <rjplott@gmail.com>
2021-02-11 20:26:39 -0500

    complete exercise 1.1

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,6 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = (props) => (
+  <div>
+    <p>
+      {props.parts[0]} {props.exercises[0]}
+    </p>
+    <p>
+      {props.parts[1]} {props.exercises[1]}
+    </p>
+    <p>
+      {props.parts[2]} {props.exercises[2]}
+    </p>
+  </div>
+);
+
+const Total = (props) => (
+  <p>
+    Number of exercises{" "}
+    {props.exercises[0] + props.exercises[1] + props.exercises[2]}
+  </p>
+);
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -12,17 +35,12 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content
+        parts={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
+      <Total exercises={[exercises1, exercises2, exercises3]} />
     </div>
   );
 };
